bc6a183614b358a76513e79d85a3563d284798c6
Delroy Brown <delroybrown.db@gmail.com>
2025-05-26 01:50:41 +0100

    upload function working

--- weddingApp_gallery/admin.py
+++ weddingApp_gallery/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Photo
 
-# Register your models here.
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "attendee",  # who uploaded
+        "uploaded_at",  # when
+        "image_preview",  # thumbnail
+    )
+    list_filter = (
+        "uploaded_at",
+        "attendee__name",
+    )
+    search_fields = ("attendee__name",)
+    readonly_fields = (
+        "uploaded_at",
+        "image_preview",
+    )
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 100px; max-width:150px; object-fit:cover;" />',
+                obj.image.url,
+            )
+        return ""
+
+    image_preview.short_description = "Preview"
